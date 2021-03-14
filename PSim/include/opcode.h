41b3f3843db2f2962ab6bf853b89b713d80b3767
Chenhao Wu <117010285@Link.cuhk.edu.cn>
2021-03-14 18:13:57 +0800

    finalize assembler

--- PSim/include/opcode.h
+++ PSim/include/opcode.h
@@ -7,10 +7,4 @@
 
 #include <map>
 
-std::map<std::string, std::vector<uint32_t>> rtype_opcode;
-std::map<std::string, std::vector<uint32_t>> itype_opcode;
-
-void init_opcode_map();
-
-
 #endif //PARCH_OPCODE_H
--- PSim/src/assembler.cc
+++ PSim/src/assembler.cc
@@ -8,9 +8,16 @@
 
 #include "assembler.hh"
 
+std::map<std::string, uint32_t> reg_map;
+
 bool isLineALabel(const std::string &line) {
     std::regex e("(.*):$");
     return std::regex_match(line, e);
+}
+
+void removeComments(std::string &line) {
+    std::regex e("#/.*?\n");
+    line = std::regex_replace(line, e, "");
 }
 
 tokens_t tokenize_str(const std::string &line) {
@@ -40,18 +47,32 @@ uint32_t __encode_reg(const token_t &reg_str) {
 
 uint32_t __encode_rtype(const tokens_t &tokens,
                         const uint32_t opcode,
-                        const uint32_t shamt,
                         const uint32_t funct) {
+
+    // Encode format:
+    // 0                   1                   2                   3
+    // 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+    //+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+    //|   opcode  |    rs   |    rt   |    rd   |  shamt  |   funct   |
+    //+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
     uint32_t rs = __encode_reg(tokens[1]);
     uint32_t rt = __encode_reg(tokens[2]);
     uint32_t rd = __encode_reg(tokens[3]);
+    uint32_t shamt = std::stoi(tokens[4]);
+    uint32_t bin = (opcode << 26) | (rs << 21) | (rt << 16) |
+                   (rd << 11) | (shamt << 6) | funct;
+    PRINTF_DEBUG_VERBOSE(verbose,
+                         "[ASM]\t[ENCODE]\tInstruction: %s  %s,%s,%s [%d]"
+                         "\t----->\t%s\n",
+                         tokens[0].c_str(),
+                         tokens[1].c_str(),
+                         tokens[2].c_str(),
+                         tokens[3].c_str(),
+                         shamt,
+                         std::bitset<32>(bin).to_string().c_str());
 
-    return (opcode << 26) |
-           (rs << 21) |
-           (rt << 16) |
-           (rd << 11) |
-           (shamt << 6) |
-           funct;
+    return bin;
 }
 
 uint32_t __encode_jtype(const tokens_t &tokens, const uint32_t opcode) {
@@ -59,26 +80,40 @@ uint32_t __encode_jtype(const tokens_t &tokens, const uint32_t opcode) {
 }
 
 uint32_t __encode_itype(const tokens_t &tokens, const uint32_t opcode) {
+
+    // Encode format:
+    // 0                   1                   2                   3
+    // 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+    //+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+    //|   opcode  |    rs   |    rt   |              imm              |
+    //+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
     uint32_t rs = __encode_reg(tokens[1]);
     uint32_t rt = __encode_reg(tokens[2]);
     uint32_t immediate = std::stoi(tokens[3]);
-
-    return (opcode << 26) |
-           (rs << 21) |
-           (rt << 16) |
-           immediate;
+    uint32_t bin = (opcode << 26) | (rs << 21) | (rt << 16) | immediate;
+    PRINTF_DEBUG_VERBOSE(verbose,
+                         "[ASM]\t[ENCODE]\tInstruction: %s  %s,%s"
+                         "\t----->\t%s\n",
+                         tokens[0].c_str(),
+                         tokens[1].c_str(),
+                         tokens[2].c_str(),
+                         std::bitset<32>(bin).to_string().c_str());
+    return bin;
 }
 
-bool encode(Assembler *assembler, const tokens_t &tokens, uint32_t *bin) {
+bool encode(Assembler *assembler, tokens_t &tokens, uint32_t *bin, uint32_t *pointat) {
     token_t opcode_string = tokens[0];
     switch (hash(opcode_string.c_str())) {
 
         case hash("add"):
-            *bin = __encode_rtype(tokens, 0x0, 0x0, 0x20);
+            tokens.push_back(0x0);
+            *bin = __encode_rtype(tokens, 0x0, 0x20);
             break;
 
         case hash("addu"):
-            *bin = __encode_rtype(tokens, 0x0, 0x0, 0x21);
+            tokens.push_back(0x0);
+            *bin = __encode_rtype(tokens, 0x0, 0x21);
             break;
 
         case hash("addi"):
@@ -90,7 +125,8 @@ bool encode(Assembler *assembler, const tokens_t &tokens, uint32_t *bin) {
             break;
 
         case hash("and"):
-            *bin = __encode_rtype(tokens, 0x0, 0x0, 0x24);
+            tokens.push_back(0x0);
+            *bin = __encode_rtype(tokens, 0x0, 0x24);
             break;
 
         case hash("andi"):
@@ -98,217 +134,306 @@ bool encode(Assembler *assembler, const tokens_t &tokens, uint32_t *bin) {
             break;
 
         case hash("clo"):
-            *bin = __encode_rtype(tokens, 0x1c, 0x0, 0x21);
+            tokens.push_back(0x0);
+            *bin = __encode_rtype(tokens, 0x1c, 0x21);
             break;
 
         case hash("clz"):
+            tokens.push_back(0x0);
+            *bin = __encode_rtype(tokens, 0x1c, 0x20);
             break;
 
         case hash("div"):
+            tokens.push_back(token_t("0x1a"));
+            *bin = __encode_itype(tokens, 0x0);
             break;
 
         case hash("divu"):
+            tokens.push_back(token_t("0x1b"));
+            *bin = __encode_itype(tokens, 0x0);
             break;
 
         case hash("mult"):
+            tokens.push_back(token_t("0x18"));
+            *bin = __encode_itype(tokens, 0x0);
             break;
 
         case hash("madd"):
+            tokens.push_back(token_t("0x0"));
+            *bin = __encode_itype(tokens, 0x1c);
             break;
 
         case hash("msub"):
+            tokens.push_back(token_t("0x4"));
+            *bin = __encode_itype(tokens, 0x1c);
             break;
 
         case hash("maddu"):
+            tokens.push_back(token_t("0x1"));
+            *bin = __encode_itype(tokens, 0x1c);
             break;
 
         case hash("msubu"):
+            tokens.push_back(token_t("0x5"));
+            *bin = __encode_itype(tokens, 0x1c);
             break;
 
         case hash("nor"):
+            tokens.push_back(0x0);
+            *bin = __encode_rtype(tokens, 0x0, 0x27);
             break;
 
         case hash("or"):
+            tokens.push_back(0x0);
+            *bin = __encode_rtype(tokens, 0x0, 0x25);
             break;
 
         case hash("ori"):
+            *bin = __encode_itype(tokens, 0xd);
             break;
 
         case hash("sll"):
+            *bin = __encode_rtype(tokens, 0x0, 0x0);
             break;
 
         case hash("sllv"):
+            tokens.push_back("0x0");
+            *bin = __encode_rtype(tokens, 0x0, 0x4);
             break;
 
         case hash("sra"):
+            *bin = __encode_rtype(tokens, 0x0, 0x3);
             break;
 
         case hash("srav"):
+            tokens.push_back("0x0");
+            *bin = __encode_rtype(tokens, 0x0, 0x7);
             break;
 
         case hash("srl"):
+            *bin = __encode_rtype(tokens, 0x0, 0x2);
             break;
 
         case hash("srlv"):
+            tokens.push_back("0x0");
+            *bin = __encode_rtype(tokens, 0x0, 0x6);
             break;
 
         case hash("sub"):
+            tokens.push_back("0x0");
+            *bin = __encode_rtype(tokens, 0x0, 0x22);
             break;
 
         case hash("subu"):
+            tokens.push_back("0x0");
+            *bin = __encode_rtype(tokens, 0x0, 0x23);
             break;
 
         case hash("xor"):
+            tokens.push_back("0x0");
+            *bin = __encode_rtype(tokens, 0x0, 0x26);
             break;
 
         case hash("xori"):
+            *bin = __encode_itype(tokens, 0xe);
             break;
 
         case hash("lui"):
+            tokens.insert(tokens.begin() + 1, "0x0");
+            *bin = __encode_itype(tokens, 0xf);
             break;
 
         case hash("slt"):
+            tokens.push_back("0x0");
+            *bin = __encode_rtype(tokens, 0x0, 0x2a);
             break;
 
         case hash("sltu"):
+            tokens.push_back("0x0");
+            *bin = __encode_rtype(tokens, 0x0, 0x2b);
             break;
 
         case hash("slti"):
+            *bin = __encode_itype(tokens, 0xa);
             break;
 
         case hash("sltiu"):
+            *bin = __encode_itype(tokens, 0xb);
             break;
 
         case hash("beq"):
+            *bin = 0;
             break;
 
         case hash("bgez"):
+            *bin = 0;
             break;
 
         case hash("bgezal"):
+            *bin = 0;
             break;
 
         case hash("bgtz"):
+            *bin = 0;
             break;
 
         case hash("blez"):
+            *bin = 0;
             break;
 
         case hash("bltzal"):
+            *bin = 0;
             break;
 
         case hash("bltz"):
+            *bin = 0;
             break;
 
         case hash("bne"):
+            *bin = 0;
             break;
 
         case hash("j"):
+            *bin = 0;
             break;
 
         case hash("jal"):
+            *bin = 0;
             break;
 
         case hash("jalr"):
+            *bin = 0;
             break;
 
         case hash("jr"):
+            *bin = 0;
             break;
 
         case hash("teq"):
+            *bin = 0;
             break;
 
         case hash("teqi"):
+            *bin = 0;
             break;
 
         case hash("tne"):
+            *bin = 0;
             break;
 
         case hash("tnei"):
+            *bin = 0;
             break;
 
         case hash("tge"):
+            *bin = 0;
             break;
 
         case hash("tgeu"):
+            *bin = 0;
             break;
 
         case hash("tgei"):
+            *bin = 0;
             break;
 
         case hash("tgeiu"):
+            *bin = 0;
             break;
 
         case hash("tlt"):
+            *bin = 0;
             break;
 
         case hash("tltu"):
+            *bin = 0;
             break;
 
         case hash("tlti"):
+            *bin = 0;
             break;
 
         case hash("tltiu"):
+            *bin = 0;
             break;
 
         case hash("lb"):
+            *bin = 0;
             break;
 
         case hash("lbu"):
+            *bin = 0;
             break;
 
         case hash("lh"):
+            *bin = 0;
             break;
 
         case hash("lhu"):
+            *bin = 0;
             break;
 
         case hash("lw"):
+            *bin = 0;
             break;
 
         case hash("lwl"):
+            *bin = 0;
             break;
 
         case hash("lwr"):
+            *bin = 0;
             break;
 
         case hash("ll"):
+            *bin = 0;
             break;
 
         case hash("sb"):
+            *bin = 0;
             break;
 
         case hash("sh"):
+            *bin = 0;
             break;
 
         case hash("sw"):
+            *bin = 0;
             break;
 
         case hash("swl"):
+            *bin = 0;
             break;
 
         case hash("swr"):
+            *bin = 0;
             break;
 
         case hash("sc"):
+            *bin = 0;
             break;
 
         case hash("mfhi"):
+            *bin = 0;
             break;
 
         case hash("mflo"):
+            *bin = 0;
             break;
 
         case hash("mthi"):
+            *bin = 0;
             break;
 
         case hash("mtlo"):
+            *bin = 0;
             break;
 
         case hash("syscall"):
             break;
 
         default:
+            PRINTF_ERR_STAMP("[!] unrecognized instruction: %s\n", opcode_string.c_str());
             return 0;
 
     }
@@ -318,6 +443,8 @@ bool encode(Assembler *assembler, const tokens_t &tokens, uint32_t *bin) {
 
 bool __assembler_exec(Assembler *assembler) {
     bool inText = false, inData = false;
+    uint32_t pointat = 0x10000;
+
     for (std::string line: assembler->content) {
         // remove leading and trailing spaces
         line = std::regex_replace(line, std::regex("^[ \t]+"), "");
@@ -329,13 +456,13 @@ bool __assembler_exec(Assembler *assembler) {
                 PRINTF_DEBUG_VERBOSE(verbose, "[ASM]\t[DT]\t\t%s\n", line.c_str());
                 inText = true;
                 inData = false;
-                break;
+                continue;
 
             case hash(".data"):
                 PRINTF_DEBUG_VERBOSE(verbose, "[ASM]\t[DD]\t\t%s\n", line.c_str());
                 inText = false;
                 inData = true;
-                break;
+                continue;
 
             default:
                 break;
@@ -345,14 +472,13 @@ bool __assembler_exec(Assembler *assembler) {
         if (inText) {
             if (isLineALabel(line)) {
                 std::string label = line.substr(0, line.find(":"));
-                PRINTF_DEBUG_VERBOSE(verbose, "[ASM]\t[LABEL]\t\t%s\n", label.c_str());
-                // do something here
-
+                PRINTF_DEBUG_VERBOSE(verbose, "[ASM]\t[LABEL]\t\t%s\t----->\tpoint_at: 0x%X\n", label.c_str(), pointat);
+                assembler->label_map[label] = pointat;
             } else {
                 tokens_t tokens = tokenize_str(line);
                 uint32_t bin_line;
 
-                if (!encode(assembler, tokens, &bin_line)) {
+                if (!encode(assembler, tokens, &bin_line, &pointat)) {
                     return 0;
                 }
             }
