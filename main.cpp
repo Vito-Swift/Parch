eb3c8776c1ae192b806ec520e5a4ad15fd407c30
Chenhao Wu <117010285@Link.cuhk.edu.cn>
2021-03-21 21:07:05 +0800

    Update for submission

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
-#include <iostream>
+/**
+ * @filename: main.cpp
+ * @author: Vito Wu <chenhaowu[at]link.cuhk.edu.cn>
+ * @version:
+ * @desc:
+ * @date: 3/9/2021
+ */
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+#include "psim.hh"
+
+int main(int argc, char** argv) {
+    Simulator simulator;
+    simulator_init(&simulator, argc, argv);
+    simulator_exec(&simulator);
+    simulator_free(&simulator);
 }
